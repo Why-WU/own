dace001c48ee3e2e15e6211b0c444ff80e64f95f
Why-WU <wuhanyu001@126.com>
2019-02-06 18:47:36 +0800

    java基础

---
+++ javaTest/helloworld.java
@@ -0,0 +1,26 @@
+/**
+*文档注释
+    笔记：
+    public 关键字 (公共的)
+    class 关键字 代表类
+    helloworld 代表一个类名(必须和文件名相同)
+    {} 代表作用域
+*/
+
+package javaTest;
+
+public class helloworld {
+    /* 
+    多行注释
+    static 关键字 静态的
+    void(空) 代表函数的返回类型
+    main(参数) 代表函数名，(main代表主函数)
+    String[] 参数是一个String类型的数组
+    args 形参的名称
+    */
+    public static void main(String[] args){
+        //单行注释
+        //在控制台输出(hello world!!)
+        System.out.print("Hello World!!");
+    }
+}
