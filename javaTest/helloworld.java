8009fbe71b4f03f999a8b8ca38647f7983b94540
Why-WU <wuhanyu001@126.com>
2019-02-07 02:34:43 +0800

    java2

--- javaTest/helloworld.java
+++ javaTest/helloworld.java
@@ -10,17 +10,13 @@
 package javaTest;
 
 public class helloworld {
-    /* 
-    多行注释
-    static 关键字 静态的
-    void(空) 代表函数的返回类型
-    main(参数) 代表函数名，(main代表主函数)
-    String[] 参数是一个String类型的数组
-    args 形参的名称
-    */
-    public static void main(String[] args){
-        //单行注释
-        //在控制台输出(hello world!!)
-        System.out.print("Hello World!!");
+    /*
+     * 多行注释 static 关键字 静态的 void(空) 代表函数的返回类型 main(参数) 
+     * 表函数名，(main代表主函数) String[] 参数是一个String类型的数组 args 形参的名称
+     */
+    public static void main(String[] args) {
+        // 单行注释
+        // 在控制台输出(hello world!!)
+        System.out.print("Hello World123!!");
     }
 }
---
+++ javaTest/test2.java
@@ -0,0 +1,11 @@
+package javaTest;
+
+public class test2 {
+    private int c = 30;           //成员变量
+    public static void main (String[] args){
+        int a = 10;                  //内部变量
+        int b = 20;
+        int myAdd = 50;
+        System.out.print(a);
+    }
+}
