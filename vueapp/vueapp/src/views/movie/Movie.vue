753a3f1c1cb90d548c2481f209f405f14c058eb4
Why-WU <wuhanyu001@126.com>
2019-02-15 12:04:00 +0800

    vueapp

--- vueapp/vueapp/src/views/movie/Movie.vue
+++ vueapp/vueapp/src/views/movie/Movie.vue
@@ -1,19 +1,35 @@
 <template>
-    <div>
-        <h1>movie</h1>
-    </div>
+  <div>
+    <h1>movie</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    created() {
-        this.$emit("matchTab","movie")
-    },
-}
+  created() {
+    this.$emit("matchTab", "movie");
+    this.getData();
+  },
+  methods: {
+    getData() {
+      let proxy = "https://bird.ioliu.cn/v1?url=";
+      let url =
+        "https://api.douban.com/v2/movie/in_theaters?city=广州&start=0&count=10";
+      axios
+        .get(proxy + url)
+        .then(function(res) {
+            console.log(res)
+        })
+        .catch(function(res) {
+          console.log(0);
+        });
+    }
+  }
+};
 </script>
 
 <style>
-    div{
-        margin-top: 50%;
-    }
+div {
+  margin-top: 50%;
+}
 </style>
