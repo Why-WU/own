bd0fee38f33640f5ca3c1662d0a466332b9bd664
Why-WU <wuhanyu001@126.com>
2019-02-16 17:04:34 +0800

    vueapp

--- vueapp/vueapp/src/views/talk/Talk.vue
+++ vueapp/vueapp/src/views/talk/Talk.vue
@@ -1,13 +1,40 @@
 <template>
-    <div>
-        <h1>talk</h1>
+  <div>
+    <div class="talk-bar">
+      <ul>
+        <li></li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    created() {
-        this.$emit("matchTab","talk")
+  created() {
+    this.$emit("matchTab", "talk");
+  },
+  methods: {
+    getTimeStamp() {
+      let timestamp = Date.parse(new Date()).toString.substr(0, 10);
+      return timestamp;
     },
-}
+    getNonceStr(){
+       let noncestr = Math.random().toString(36).substr(2);
+       return noncestr;
+    },
+    getSign(){
+        
+    },
+    send() {
+      let params = {
+        app_id: 2111975625,
+        time_stamp:getTimeStamp(),
+        nonce_str:getNonceStr(),
+        session:10000,
+        question:"空",
+        sign
+      };
+    }
+  }
+};
 </script>
